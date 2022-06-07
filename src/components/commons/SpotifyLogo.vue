2e3a5470a7b177f20c37c0007c21ab77cfc5be2b
TrvMarco <trevisanello.marco@gmail.com>
2022-06-07 16:22:39 +0200

    Creato bozza Header

---
+++ src/components/commons/SpotifyLogo.vue
@@ -0,0 +1,13 @@
+<template>
+    <img class="img-fluid" id="Spoty_Logo" src="../../assets/img/spotify-logo.png" alt="Spotify Logo">
+</template>
+
+<script>
+export default {
+    name: 'SpotifyLogo'
+}
+</script>
+
+<style lang="scss">
+    
+</style>
---
+++ src/components/macro/BaseHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <div class="container-fluid">
+        <SpotifyLogo/>
+    </div>
+  </header>
+</template>
+
+<script>
+import SpotifyLogo from '../commons/SpotifyLogo.vue'
+export default {
+    name: 'BaseHeader',
+    components:{
+        SpotifyLogo
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        height: 75px;
+        background-color: var(--primary-color);
+    }
+</style>
